72ae60e8f47948f577c1d14e040d037f85cbb416
bartek <bartekwichowski@gmail.com>
2019-02-18 00:34:16 +0100

    not needed

--- backend/target/classes/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO todo (task, date_created) VALUES ('Some task 1', '2010-01-01 11:00:00+03');
-INSERT INTO todo (task, date_created) VALUES ('Some task 2', '2010-01-01 11:00:00+03');
-INSERT INTO todo (task, date_created) VALUES ('Some task 3', '2010-01-01 11:00:00+03');
-INSERT INTO todo (task, date_created) VALUES ('Some task 4', '2010-01-01 11:00:00+03');
